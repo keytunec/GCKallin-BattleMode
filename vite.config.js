ff868c083fc67b428a92c7ee1f18e22cbf161e04
keytunec <gorlachillya@gmail.com>
2026-01-19 16:28:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GCKallin-BattleMode/', // MUSS exakt so heißen wie dein Repository auf GitHub
+  base: '/GCKallin-BattleMode/',
 })
